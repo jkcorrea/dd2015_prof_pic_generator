3af0b5670197e26471c4432100afd4c799a3cb69
jkcorrea <jcorrea257@gmail.com>
2015-09-28 15:16:37 -0400

    Update with micah's artwork

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var multer = require('multer');
-var images = require('images');
 var fs = require("fs");
+
+var Canvas = require('canvas'),
+    Image = Canvas.Image;
 
 var app = express();
 var storage = multer.memoryStorage();
@@ -21,43 +23,54 @@ app.get('/', function(req, res) {
 
 app.post('/render', upload.single('prof-pic-original'), function(req, res, next) {
   if (!req.file) return res.end("Please upload a profile picture to donut-ify.");
-  _render(res, req.file);
+  _render(res, req.file, req.body['overlay-type-selector']);
 });
 
-app.listen(env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Working on port 3000");
 });
 
 
 // Combining images
 var donut_overlay;
-fs.readFile(__dirname + '/public/donut_overlay.png', function(err, file) {
+fs.readFile(__dirname + '/public/overlay_dark.png', function(err, file) {
   if (err) throw "Could not load overlay image!";
-  donut_overlay = images(file);
+  overlay_dark = file;
+});
+fs.readFile(__dirname + '/public/overlay_light.png', function(err, file) {
+  if (err) throw "Could not load overlay image!";
+  overlay_light = file;
 });
 
-var _render = function(res, file) {
-  var base, base_width, base_height;
-  try { // First make sure we can read file as an image
-    base = images(file.buffer);
-  } catch (e) {
-    return res.end("Upload failed, please try again with another image");
-  }
-  base_width = base.width();
-  base_height = base.height();
+var _render = function(res, file, theme) {
+  var fail = function(err) { res.end("Upload failed, please try again with another image"); }
 
+  var base = new Image
+
+  base.onerror = fail;
+  base.onload = function() {
   // Check that the image is correct proportions
-  if (base_width !== base_height) return res.end("Please only use images that are square!");
-  if (base_width < 180) return res.end("Image must be at least 180x180 pixels!");
+    if (base.width !== base.height) return res.end("Please only use images that are square!");
+    if (base.width < 500) return res.end("Image must be at least 500x500 pixels!");
 
-  // Merge images and send response
-  var overlay = images(donut_overlay).resize(base_width / 2);
-  var merged = base
-        .draw(overlay, 0, base_height - overlay.height())
-        .encode("jpg", { quality: 99 });
-  // Send...
-  res.setHeader('Content-Type', 'image/jpeg');
-  res.setHeader('Content-disposition', 'attachment; filename=donut_dash_prof_pic.jpg');
-  res.write(merged, 'binary');
-  res.end();
+    var overlay = new Image;
+    overlay.onerror = fail;
+    overlay.onload = function() {
+      var canvas = new Canvas(overlay.width, overlay.height);
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(base, 0, 0, overlay.width, overlay.height);
+      ctx.drawImage(overlay, 0, 0, overlay.width, overlay.height);
+
+      // Send...
+      var merged = canvas.toBuffer();
+      res.setHeader('Content-Type', 'image/png');
+      res.setHeader('Content-disposition', 'attachment; filename=donut_dash_prof_pic.png');
+      res.write(merged, 'binary');
+      res.end();
+    };
+
+    overlay.src = (theme === 'dark') ? overlay_dark : overlay_light;
+  };
+
+  base.src = file.buffer;
 };
--- public/index.js
+++ public/index.js
@@ -2,3 +2,8 @@ function runPreview(e) {
   var output = document.getElementById('prof-pic-preview');
   output.src = URL.createObjectURL(e.target.files[0]);
 }
+
+function changeType() {
+  document.getElementById('overlay-dark').classList.toggle('hide');
+  document.getElementById('overlay-light').classList.toggle('hide');
+}
